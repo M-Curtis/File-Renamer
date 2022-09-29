a1d120705d7978f95e87db35b80f01935cfc6666
Curtis Eden Mullett <curtis.mullett@gmail.com>
2022-09-29 11:42:04 +0100

    Updated .NET target to 4.8 Added new folder picker to rename directories useful for nested subtitle folders

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,123 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lstBxScannedItems = new System.Windows.Forms.ListBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.lstBxNewItems = new System.Windows.Forms.ListBox();
-            this.btnChange = new System.Windows.Forms.Button();
-            this.txtBxName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtBxExtension = new System.Windows.Forms.TextBox();
-            this.menuStrip1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // lstBxScannedItems
-            // 
-            this.lstBxScannedItems.FormattingEnabled = true;
-            this.lstBxScannedItems.Location = new System.Drawing.Point(12, 41);
-            this.lstBxScannedItems.Name = "lstBxScannedItems";
-            this.lstBxScannedItems.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.lstBxScannedItems.Size = new System.Drawing.Size(642, 303);
-            this.lstBxScannedItems.TabIndex = 10;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.lstBxScannedItems = new System.Windows.Forms.ListBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.lstBxNewItems = new System.Windows.Forms.ListBox();
+			this.btnChange = new System.Windows.Forms.Button();
+			this.txtBxName = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtBxExtension = new System.Windows.Forms.TextBox();
+			this.openFolderSUBSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lstBxScannedItems
+			// 
+			this.lstBxScannedItems.FormattingEnabled = true;
+			this.lstBxScannedItems.Location = new System.Drawing.Point(12, 41);
+			this.lstBxScannedItems.Name = "lstBxScannedItems";
+			this.lstBxScannedItems.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+			this.lstBxScannedItems.Size = new System.Drawing.Size(642, 303);
+			this.lstBxScannedItems.TabIndex = 10;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1381, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openFolderToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
-            // 
-            // openFolderToolStripMenuItem
-            // 
-            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
-            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.openFolderToolStripMenuItem.Text = "Open Folder";
-            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.OpenFolderToolStripMenuItem_Click);
-            // 
-            // lstBxNewItems
-            // 
-            this.lstBxNewItems.FormattingEnabled = true;
-            this.lstBxNewItems.Location = new System.Drawing.Point(702, 41);
-            this.lstBxNewItems.Name = "lstBxNewItems";
-            this.lstBxNewItems.Size = new System.Drawing.Size(667, 303);
-            this.lstBxNewItems.TabIndex = 10;
-            // 
-            // btnChange
-            // 
-            this.btnChange.Location = new System.Drawing.Point(640, 396);
-            this.btnChange.Name = "btnChange";
-            this.btnChange.Size = new System.Drawing.Size(75, 23);
-            this.btnChange.TabIndex = 3;
-            this.btnChange.Text = "Change";
-            this.btnChange.UseVisualStyleBackColor = true;
-            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
-            // 
-            // txtBxName
-            // 
-            this.txtBxName.Location = new System.Drawing.Point(392, 399);
-            this.txtBxName.Name = "txtBxName";
-            this.txtBxName.Size = new System.Drawing.Size(142, 20);
-            this.txtBxName.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(318, 402);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Show Name:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(318, 376);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "File Extension:";
-            // 
-            // txtBxExtension
-            // 
-            this.txtBxExtension.Location = new System.Drawing.Point(392, 373);
-            this.txtBxExtension.Name = "txtBxExtension";
-            this.txtBxExtension.Size = new System.Drawing.Size(142, 20);
-            this.txtBxExtension.TabIndex = 0;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1381, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtBxExtension);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtBxName);
-            this.Controls.Add(this.btnChange);
-            this.Controls.Add(this.lstBxNewItems);
-            this.Controls.Add(this.lstBxScannedItems);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1381, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem,
+            this.openFolderSUBSToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// openFolderToolStripMenuItem
+			// 
+			this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+			this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.openFolderToolStripMenuItem.Text = "Open Folder";
+			this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.OpenFolderToolStripMenuItem_Click);
+			// 
+			// lstBxNewItems
+			// 
+			this.lstBxNewItems.FormattingEnabled = true;
+			this.lstBxNewItems.Location = new System.Drawing.Point(702, 41);
+			this.lstBxNewItems.Name = "lstBxNewItems";
+			this.lstBxNewItems.Size = new System.Drawing.Size(667, 303);
+			this.lstBxNewItems.TabIndex = 10;
+			// 
+			// btnChange
+			// 
+			this.btnChange.Location = new System.Drawing.Point(640, 396);
+			this.btnChange.Name = "btnChange";
+			this.btnChange.Size = new System.Drawing.Size(75, 23);
+			this.btnChange.TabIndex = 3;
+			this.btnChange.Text = "Change";
+			this.btnChange.UseVisualStyleBackColor = true;
+			this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+			// 
+			// txtBxName
+			// 
+			this.txtBxName.Location = new System.Drawing.Point(392, 399);
+			this.txtBxName.Name = "txtBxName";
+			this.txtBxName.Size = new System.Drawing.Size(142, 20);
+			this.txtBxName.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(318, 402);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Show Name:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(318, 376);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(75, 13);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "File Extension:";
+			// 
+			// txtBxExtension
+			// 
+			this.txtBxExtension.Location = new System.Drawing.Point(392, 373);
+			this.txtBxExtension.Name = "txtBxExtension";
+			this.txtBxExtension.Size = new System.Drawing.Size(142, 20);
+			this.txtBxExtension.TabIndex = 0;
+			// 
+			// openFolderSUBSToolStripMenuItem
+			// 
+			this.openFolderSUBSToolStripMenuItem.Name = "openFolderSUBSToolStripMenuItem";
+			this.openFolderSUBSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.openFolderSUBSToolStripMenuItem.Text = "Open Folder (SUBS)";
+			this.openFolderSUBSToolStripMenuItem.Click += new System.EventHandler(this.openFolderSUBSToolStripMenuItem_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1381, 450);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtBxExtension);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtBxName);
+			this.Controls.Add(this.btnChange);
+			this.Controls.Add(this.lstBxNewItems);
+			this.Controls.Add(this.lstBxScannedItems);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -160,6 +169,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtBxExtension;
-    }
+		private System.Windows.Forms.ToolStripMenuItem openFolderSUBSToolStripMenuItem;
+	}
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,6 +9,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        bool GFolder = false;
         public Form1()
         {
             InitializeComponent();
@@ -27,7 +28,6 @@ namespace WindowsFormsApp1
             }
             //Looks at previous file names and creates a new
             var startItems = lstBxScannedItems.Items;
-            var items = new string[lstBxScannedItems.Items.Count];
             foreach (string item in startItems)
             {
                 var folder = item.Substring(0, item.LastIndexOf('\\') + 1);
@@ -36,23 +36,59 @@ namespace WindowsFormsApp1
                 var newItem = $"{folder}{txtBxName.Text} - {episode.ToUpper()}.{txtBxExtension.Text}";
                 lstBxNewItems.Items.Add(newItem);
             }
+            GFolder = false;
+        }
+        private void openFolderSUBSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lstBxScannedItems.Items.Clear();
+            lstBxNewItems.Items.Clear();
+            using (CommonOpenFileDialog dialog = new CommonOpenFileDialog { InitialDirectory = @"D:\Media\", IsFolderPicker = true })
+			{
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    lstBxScannedItems.Items.AddRange(Directory.GetDirectories(dialog.FileName));
+                }
+			}
+            var startItems = lstBxScannedItems.Items;
+            foreach (string item in startItems)
+            {
+                var folder = item.Substring(0, item.LastIndexOf('\\') + 1);
+                var file = item.Substring(item.LastIndexOf('\\') + 1);
+                var episode = Regex.Match(file, "[Ss]\\d\\d[Ee]\\d\\d+").Value;
+                var newItem = $"{folder}{txtBxName.Text} - {episode.ToUpper()}";
+                lstBxNewItems.Items.Add(newItem);
+            }
+            GFolder = true;
         }
 
         private void btnChange_Click(object sender, EventArgs e)
         {
             try
             {
-                for (int i = 0; i < lstBxScannedItems.Items.Count; i++)
-                {
-                    File.Move(lstBxScannedItems.Items[i].ToString(), lstBxNewItems.Items[i].ToString());
+                if(GFolder == true)
+				{
+                    for (int i = 0; i < lstBxScannedItems.Items.Count; i++)
+                    {
+                        Directory.Move(lstBxScannedItems.Items[i].ToString(), lstBxNewItems.Items[i].ToString());
+                    }
                 }
+				else
+				{
+                    for (int i = 0; i < lstBxScannedItems.Items.Count; i++)
+                    {
+                        File.Move(lstBxScannedItems.Items[i].ToString(), lstBxNewItems.Items[i].ToString());
+                    }
+                }
+                
             }
             catch
             {
-                MessageBox.Show("Failed to rename files","FAILED!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Failed to rename ?{(GFolder ? "files" : "directories")}","FAILED!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            MessageBox.Show("All Files renamed successfully", "Complete",MessageBoxButtons.OK);
+            MessageBox.Show($"All {(GFolder ? "directories" : "files")} renamed successfully", "Complete",MessageBoxButtons.OK);
         }
-    }
+
+
+	}
 }
